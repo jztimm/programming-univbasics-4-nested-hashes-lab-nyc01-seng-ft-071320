54983b8ddbee553c204d7e735cbc2af93ddb8930
Jzavier Timm <jztimm@gmail.com>
2020-07-02 20:16:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,7 +62,7 @@ def dennis_ritchies_language
       }
     }
     
-    puts programmer_hash[:dennis_ritchie][:languages[0]]
+    puts programmer_hash[:dennis_ritchie][:languages][0]
     programmer_hash[:dennis_ritchie][:languages][0]
 
 end
