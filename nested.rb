c2eca7c517d576509571c51fe888410c896a24d7
Jzavier Timm <jztimm@gmail.com>
2020-07-02 20:18:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,6 +90,13 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:yukihiro_matsumoto] = {
+      know_for: "Ruby",
+      languages: ["LISP", "C"]
+    }
+    
+    programmer_hash
 
 end
 
